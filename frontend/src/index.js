17c40254f7d4685976f640c86c2608ebe032d44b
Siddhartha <92244610+Witty-31-06@users.noreply.github.com>
2025-03-28 22:51:50 +0530

    changed the basename

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+const basename = "/Uncode-website"; // Your repo name
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={basename}>
     <App />
   </BrowserRouter>
 );
