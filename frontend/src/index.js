a9cbfa27cf78bb133e826e4cb226781beb4cf03b
Siddhartha <92244610+Witty-31-06@users.noreply.github.com>
2025-03-29 00:05:20 +0530

    removed basename for final hosting

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-const basename = "/Uncode-website"; // Your repo name
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename={basename}>
+  <BrowserRouter basename>
     <App />
   </BrowserRouter>
 );
